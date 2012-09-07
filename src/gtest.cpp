e1ac6fdb11e9ad61f370606f7e1c259b77ea14aa
AlexanderLi <ljg_emails@sina.com>
2012-09-07 23:04:54 +0800

    Init project. Add include, src and README.

---
+++ src/gtest.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : gtest.cpp
+// Author      : Jingang Li
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "GTest" << endl; // prints GTest
+	return 0;
+}
